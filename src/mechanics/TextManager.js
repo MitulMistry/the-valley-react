e614a61da967eae923a190e1b91547c37d96e9b7
Mitul Mistry <MitulMistryDev@gmail.com>
2021-10-14 15:56:44 -0500

    Update TextManager to load JSON story data into objects structured with keys

--- src/mechanics/TextManager.js
+++ src/mechanics/TextManager.js
@@ -7,48 +7,50 @@ import constants from '../globals/constants';
 
 export default class {
 
-  // This method returns an object structured like this:
+  // This method loads JSON story data and returns an object
+  // structured like this:
   // {
   //   textData: data,
   //   linkNodesData: data,
   //   choicesData: data
   // }
   static loadModule(moduleNumber) {
-    console.log('%c Loading story module ' + moduleNumber + ' ', 'color:white; background:orange;');
+    console.log('%c Loading story module ' + moduleNumber + '... ', 'color:white; background:orange;');
 
     if (moduleNumber === constants.MODULE_ASCENT_OF_MAN) {
       return this.loadJSON(ascentOfManTextJSON, ascentOfManChoicesJSON, ascentOfManLinkNodesJSON);
     } else {
-      console.log('%cError: Invalid story module.', 'color:white; background:red;');
+      console.log('%c Error: Invalid story module. ', 'color:white; background:red;');
       return {};
     }
   }
 
-  // Use this method to asynchronously load the JSON data.
+  // Use this method to asynchronously load the JSON story data.
   // It returns a promise, so it can be chained with .then() or .catch().
   static loadModuleAsync(moduleNumber) {
-    console.log('%c Loading story module ' + moduleNumber + ' ', 'color:white; background:orange;');
+    console.log('%c Loading story module ' + moduleNumber + '... ', 'color:white; background:orange;');
 
     return new Promise((resolve, reject) => {
       if (moduleNumber === constants.MODULE_ASCENT_OF_MAN) {
         const data = this.loadJSON(ascentOfManTextJSON, ascentOfManChoicesJSON, ascentOfManLinkNodesJSON);
                 
         if(data) {
-          console.log(data);
+          console.log('%c Successfully loaded story module ' + moduleNumber + '. ', 'color:white; background:green;');
           resolve(data);
         } else {
-          console.log('%cError: Failed to load story module.', 'color:white; background:red;');
+          console.log('%c Error: Failed to load story module. ', 'color:white; background:red;');
           reject({error: 'Failed to load story module'});
         }        
 
       } else {
-        console.log('%cError: Invalid story module.', 'color:white; background:red;');
+        console.log('%c Error: Invalid story module. ', 'color:white; background:red;');
         reject({error: 'Invalid story module'});
       }
     });
   }
 
-  // This method is used to structure and return the JSON data.
+  // This method is used to structure and return the JSON story data.
+  // It sets the data keys (such as AA000AA000AA) to be keys in the objects.
   // Don't use this method directly. Use either loadModule() or loadModuleAsync().
   // (Create React App is not currently configured to support private methods).
   static loadJSON(moduleText, moduleChoices, moduleLinkNodes) {
@@ -58,24 +60,17 @@ export default class {
         choicesData: {}
       }
 
-    // for (var i in moduleText) {
-    //   data.textData.set(moduleText[i].KEY, moduleText[i].TEXT);
-    //   // data.textData[moduleText[i].KEY] = moduleText[i].TEXT;
-    // }
+    for (const i in moduleText) {
+      data.textData[moduleText[i].KEY] = moduleText[i].TEXT;
+    }
 
-    // let currentModuleChoicesMap = new Map();
-    // for (var j in moduleChoices) {
-    //   currentModuleChoicesMap.set(moduleChoices[j].KEY, moduleChoices[j]);
-    // }
+    for (const j in moduleChoices) {
+      data.choicesData[moduleChoices[j].KEY] = moduleChoices[j];
+    }
 
-    // let currentModuleLinkNodesMap = new Map();
-    // for (var k in moduleLinkNodes) {
-    //   currentModuleLinkNodesMap.set(moduleLinkNodes[k].KEY, moduleLinkNodes[k]);
-    // }
-
-    data.textData = moduleText;
-    data.choicesData = moduleChoices;
-    data.linkNodesData = moduleLinkNodes;
+    for (const k in moduleLinkNodes) {
+      data.linkNodesData[moduleLinkNodes[k].KEY] = moduleLinkNodes[k];
+    }
 
     return data;
   }
