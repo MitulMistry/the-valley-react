f0c6878b5611625339204cff7c949b1caa695b64
Mitul Mistry <MitulMistryDev@gmail.com>
2022-11-15 14:17:29 -0600

    Set Redux Dev Tools, Logger, and testing to be used only in development environment

--- src/store/store.js
+++ src/store/store.js
@@ -1,16 +1,32 @@
-import { createStore, applyMiddleware } from 'redux';
-import { composeWithDevTools } from '@redux-devtools/extension';
+import { createStore } from 'redux';
 import thunk from 'redux-thunk';
-import logger from 'redux-logger';
 
 import rootReducer from '../reducers/rootReducer';
 
 const configureStore = (preloadedState = {}) => {
-  const store = createStore(
-    rootReducer,
-    preloadedState,
-    composeWithDevTools(applyMiddleware(thunk, logger))
-    );
+  let buildStore;
+
+  // Add Redux Dev Tools and Logger only in development environment
+  if (process.env.NODE_ENV !== "production") {
+    // Must use 'require' (import only allowed at top of file)
+    const { applyMiddleware } = require('redux');
+    const { composeWithDevTools } = require('@redux-devtools/extension');
+    const { logger } = require('redux-logger');
+
+    buildStore = createStore(
+      rootReducer,
+      preloadedState,
+      composeWithDevTools(applyMiddleware(thunk, logger))
+      );
+
+  } else {
+    buildStore = createStore(
+      rootReducer,
+      preloadedState
+      );
+  }
+
+  const store = buildStore;
 
   store.subscribe(() => {
     let state = store.getState();
